59dd17798b39c83e5d6b0c959e54ee9b12c56787
sukeroku7 <yazakit@mac.com>
2019-02-10 16:06:07 +0900

    Initial Commit

---
+++ calc/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  calc
+//
+//  Created by 矢崎 泰明 on 2019/02/10.
+//  Copyright © 2019 karakusado. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ calc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by 矢崎 泰明 on 2019/02/10.
+//  Copyright © 2019 karakusado. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
